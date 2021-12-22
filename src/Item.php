19f93057b14cc502f7c6521d914b30847a2c5e3f
Khaled Elfishawy <khalidelfishawy@gmail.com>
2021-12-23 00:09:21 +0200

    Update Item.php

--- src/Item.php
+++ src/Item.php
@@ -53,6 +53,6 @@ class Item
         $print_priceqty = str_pad($item_price . ' x ' . $this->qty, $left_cols);
         $print_subtotal = str_pad($item_subtotal, $right_cols, ' ', STR_PAD_LEFT);
 
-        return "$print_name\n$print_priceqty$print_subtotal\n";
+        return "$print_name$print_priceqty$print_subtotal\n";
     }
 }
